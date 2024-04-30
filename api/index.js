fa856be336faddb323f1381d5ab51463387abe0a
Pedro Lima <tsc.pedrolima@gmail.com>
2024-04-30 15:29:47 -0300

    feat: add cors

--- api/index.js
+++ api/index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const app = express();
-const main = require("./db.js")
+const main = require("./db.js");
+const cors = require("cors");
 
-main()
+main();
 
-const pictureRouter = require("./routes/picture")
+app.use(cors());
+
+const pictureRouter = require("./routes/picture");
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.use("/pictures", pictureRouter)
+app.use("/pictures", pictureRouter);
 
 app.listen(8080, () => console.log("Server ready on port 8080."));
