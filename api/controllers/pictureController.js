0ac0705df2f2e0aba6566c827cd07e8a960fac8c
Pedro Lima <tsc.pedrolima@gmail.com>
2024-04-29 22:30:28 -0300

    feat: add create and delete function

--- api/controllers/pictureController.js
+++ api/controllers/pictureController.js
@@ -1,4 +1,5 @@
 const Picture = require("../models/Picture");
+const fs = require("fs");
 
 exports.findAll = async (req, res) => {
     try {
@@ -9,5 +10,43 @@ exports.findAll = async (req, res) => {
     } catch (error) {
         console.error("Error when searching for images:", error);
         res.status(500).json({ message: "Error when searching for images", error: error.message });
+    }
+}
+
+exports.create = async (req, res) => {
+    try {
+        console.log(req.file);
+        const file = req.file;
+
+        const picture = new Picture({
+            name: file.originalname,
+            src: file.path,
+        });
+        
+        await picture.save();
+
+        res.json({ picture, message: "Image salved successfully"});
+    } catch (error) {
+        console.error("Error saving image:", error);
+        res.status(500).json({ message: "Error saving image.", error: error.message });
+    }
+}
+
+exports.remove = async (req, res) => {
+    try {
+        const picture = await Picture.findById(req.params.id);
+
+        if (!picture) {
+            return res.status(404).json({ message: "Image not found"});
+        }
+
+        fs.unlinkSync(picture.src);
+        
+        await picture.deleteOne();
+
+        res.json({ message: "Image removed successfully"});
+    } catch (error) {
+        console.error("Error deleting image:", error);
+        res.status(500).json({ message: "Error deleting image.", error: error.message});
     }
 }
