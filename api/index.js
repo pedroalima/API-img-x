4f1438f9e439368a695c45291586e65513afc845
Pedro Lima <tsc.pedrolima@gmail.com>
2024-04-30 19:32:45 -0300

    feat: add cors authorization

--- api/index.js
+++ api/index.js
@@ -5,7 +5,13 @@ const cors = require("cors");
 
 main();
 
-app.use(cors());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization");
+    app.use(cors());
+    next();
+});
 
 const pictureRouter = require("./routes/picture");
 
